028ee18ecd2e6f16e64d3ce9e030a9df3303c9f8
Dasappanb <100546828+Dasappanb@users.noreply.github.com>
2023-05-03 05:31:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bmical
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-tyszgf)
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,155 @@
+<html>
+<html lang="en">
+<head>
+<title>BMI Calculator | Home</title>
+<link rel="icon" type="image/x-icon" href="/thumbnails/favicon.jpeg"/>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatiable" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="preconnect"
+href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+href="https://fonts.googleapis.com/css2?family=Barlow:
+wght@900&display=swap" rel="stylesheet">
+
+<script src="script.js"></script>
+</head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+href="https://fonts.googleapis.com/css2?family=Barlow:
+wght@900&display=swap" rel="stylesheet"><body>
+<header>
+<h1>BMI Calculator</h1>
+<h6>Be healthy</h6>
+<br>
+<hr>
+</header>
+<div class="col">
+<section>
+<div class="container">
+<p>Move the slider to find your BMI ⇄
+</p><br>
+<div class="row">
+<input type="range" min="20" max="150" value="20" id="weight" oninput="calculate()">
+<span id="weight-val">20
+KG</span>
+</div>
+<div class="row">
+<input type="range" min="100" max="230" value="100", id="height" oninput="calculate()">
+<span id="height-val">100
+CM</span>
+</div>
+<div class="res1">
+<p id="result"></p>
+<p id="category"></p>
+</div>
+</div>
+</section>
+<article>
+<h2>What is BMI</h2>
+<p>
+<b>BMI (body mass index)</b> is a
+calculated number representing a person's level of fat or
+obesity level. According to the Centers for Disease Control
+and Prevention (CDC), a BMI of 30 or above indicates
+obesity. A high BMI can indicate high body fatness. BMI
+screens for weight categories that may lead to health
+problems, but it does not diagnose the body fatness
+or health of an individual. </p><br>
+<h3>Calculating BMI: The Formula</h3>
+The formula to calculate BMI requires
+information about a person's height in meters and weight
+in kilograms. For weight
+in pounds and height in inches, there is a
+different formula, but due to rounding, it may not produce
+the exact same results. The BMI formula is:<br><br>
+<i>BMI = weight (in kilograms) / height (in
+meters) ^2</i>
+<br><br><br>
+<img src="/thumbnails/bmi-chart.png" alt="bmi chart" width="600" height="652">
+</p>
+<br>
+<h3>How is BMI used ?</h3>
+<p>
+BMI can be a screening tool, but it does not
+diagnose the body fatness or health of an individual. To determine if BMI is a health risk, a
+healthcare provider performs further assessments. Such assessments include skinfold
+thickness measurements, evaluations of diet, physical
+activity, and family history. </p>
+<br>
+<h3 class="overweight">Risks associated with
+being overweight</h3>
+<p>
+Being overweight increases the risk of a
+number of serious diseases and health conditions. Below
+is a list of said risks, according to the Centers for Disease
+Control and Prevention (CDC):
+<ul>
+<li>High blood pressure</li>
+<li>Higher levels of LDL cholesterol, which is widely considered "bad cholesterol," lower levels
+of HDL cholesterol, considered to be good cholesterol in
+moderation, and high levels of triglycerides</li>
+<li>Type II diabetes</li>
+<li>Coronary heart disease</li>
+<li>Stroke</li>
+<li>Gallbladder disease</li>
+<li>Osteoarthritis, a type of joint disease
+caused by breakdown of joint cartilage</li>
+<li>Sleep apnea and breathing
+problems</li>
+<li>Certain cancers (endometrial, breast, colon, kidney, gallbladder, liver)</li>
+<li>Low quality of life</li>
+<li>Mental illnesses such as clinical
+depression, anxiety, and others</li>
+<li>Body pains and difficulty with certain
+physical functions</li>
+<li>Generally, an increased risk of
+mortality compared to those with a healthy BMI</li>
+</ul>
+</p><br>
+<h3 class="underweight">Risks associated with
+being underweight</h3>
+<p>
+Being underweight has its own associated
+risks, listed below:
+<ul>
+<li> Malnutrition, vitamin deficiencies, anemia (lowered ability to carry blood vessels)</li>
+<li>Osteoporosis, a disease that causes
+bone weakness, increasing the risk of breaking a bone</li>
+<li>A decrease in immune function</li>
+<li>Growth and development issues, particularly in children and teenagers</li>
+<li>Possible reproductive issues for
+women due to hormonal imbalances that can disrupt the
+menstrual cycle. Underweight women also have a higher
+chance of miscarriage in the first trimester</li>
+<li>Potential complications as a result
+of surgery</li>
+<li>Generally, an increased risk of
+mortality compared to those with a healthy BMI</li>
+</ul>
+</p>
+</article>
+<br><br><br><br>
+</div>
+<footer>
+<br>
+<a href="#">Back to top</a>
+<p>created by gptc students</p>
+<details class="summary">
+<summary>
+Created by
+</summary>
+<p>
+Shijith or<br>
+anuj joy<br>
+akhila ku
+</p>
+</details>
+<br>
+<p>&lt;&lt;&lt; &copy; 2022 B M I
+&gt;&gt;&gt;</p>
+</footer>
+</body>
+</html>
